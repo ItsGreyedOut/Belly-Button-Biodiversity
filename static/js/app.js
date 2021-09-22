0acb6487824723577721e8cda84cf1fc6be21b98
Grey Hardy <greyhardy7@gmail.com>
2021-09-22 17:16:49 -0400

    writing code for app.js bubble, guage, and bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,71 @@
 function buildCharts(selectedPatientID) {
     d3.json("samples.json").then(data => {
         console.log(data)
-        // ADD APPROXIMATELY 50 LINES OF CODE
-        // Plotly.newPlot("barDiv", barData, barLayout)
-        // Plotly.newPlot("bubbleDiv", bubbleData, bubbleLayout)
-        // Plotly.newPlot("gaugeDiv", gaugeData, gaugeLayout)
+        var selectedPatientData = data.samples.filter(patientData => patientData.id == selectedPatientID)[0]
+        console.log(selectedPatientData)
+        // Plot Bar Chart
+        // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+        // Use sample_values as the values for the bar chart.
+        // Use otu_ids as the labels for the bar chart.
+        // Use otu_labels as the hovertext for the chart.
+
+        var trace1 = {
+            x: selectedPatientData,
+            y: selectedPatientData,
+            marker: {
+                color: ['rgba(204,204,204,1)', 'rgba(222,45,38,0.8)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)', 'rgba(204,204,204,1)']
+            },
+            type: 'bar'
+        };
+
+        var data = [trace1];
+
+        var layout = {
+            title: 'Least Used Feature'
+        };
+
+        Plotly.newPlot('barGraphDiv', data, layout);
+        // Plot Bubble Chart
+        // Use otu_ids for the x values.
+        // Use sample_values for the y values.
+        // Use sample_values for the marker size.
+        // Use otu_ids for the marker colors.
+        // Use otu_labels for the text values.
+        var trace1 = {
+            x: [1, 2, 3, 4],
+            y: [10, 11, 12, 13],
+            mode: 'markers',
+            marker: {
+                size: [40, 60, 80, 100]
+            }
+        };
+
+        var data = [trace1];
+
+        var layout = {
+            title: 'Marker Size',
+            showlegend: false,
+            height: 600,
+            width: 600
+        };
+
+        Plotly.newPlot('bubbleDiv', data, layout);
+
+        // Plot Ggauge
+        var data = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: 450,
+                title: { text: "Speed" },
+                type: "indicator",
+                mode: "gauge+number",
+                delta: { reference: 400 },
+                gauge: { axis: { range: [null, 500] } }
+            }
+        ];
+
+        var layout = { width: 600, height: 400 };
+        Plotly.newPlot('gaugeDiv', data, layout);
     })
 };
 
